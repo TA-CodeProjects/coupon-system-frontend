1c6425358af57100190ecb21366e4ffef6df171e
tomer <tomerab00@gmail.com>
2022-08-06 12:16:56 +0300

    remove yup

--- src/Components/AuthArea/Login/Login.tsx
+++ src/Components/AuthArea/Login/Login.tsx
@@ -6,19 +6,19 @@ import { loginAction } from "../../../Redux/AuthAppState";
 import store from "../../../Redux/store";
 import notify, { SccMsg } from "../../../Services/Notification";
 import { login } from "../../../WebApi/UsersApi";
-import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
+// import * as yup from "yup";
+// import { yupResolver } from "@hookform/resolvers/yup";
 import "./Login.css";
 import { ClientType } from "../../../Models/ClientType";
 
 function Login(): JSX.Element {
     const navigate = useNavigate();
 
-    const schema = yup.object().shape({
-      clientType: yup.string().required("Client Type is required"),
-      email: yup.string().email("Email is required"),
-      password: yup.string().min(4).max(15).required("Password is required"),
-    });
+    // const schema = yup.object().shape({
+    //   clientType: yup.string().required("Client Type is required"),
+    //   email: yup.string().email("Email is required"),
+    //   password: yup.string().min(4).max(15).required("Password is required"),
+    // });
 
     const navigation = (clientType: ClientType) => {
       switch (clientType) {
@@ -41,7 +41,7 @@ function Login(): JSX.Element {
       formState: { errors, isDirty, isValid },
     } = useForm<CredentialsModel>({
       mode: "all",
-      resolver: yupResolver(schema),
+      // resolver: yupResolver(schema),
     });
 
     const onSubmit = async (credentials: CredentialsModel) => {
